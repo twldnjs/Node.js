2eb1089f126649740f258963e203ca48733cf9d6
twldnjs <jwn032@gmail.com>
2026-02-06 20:17:36 +0900

    feat: add jwt,token,env file

--- routes/channels.js
+++ routes/channels.js
@@ -1,100 +1,160 @@
 import express from 'express';
 import conn from '../mariadb.js';
-
+import { body, param, validationResult } from 'express-validator';
 const router = express.Router();
 
 router.use(express.json());
 
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+
+  if (err.isEmpty()) {
+    return next(); // 에러가 없다면 다음 할 일(미들웨어 또는 함수) 을 실행할 수 있게 해줌
+  } else {
+    return res.status(400).json({ errors: err.array() });
+  }
+};
+
 router
   .route('/')
-  .get(async (req, res) => {
+  .get(
     // 채널 전체 조회
-    const { userId } = req.body;
+    [body('userId').notEmpty().isInt().withMessage('숫자 입력 필요'), validate],
+    async (req, res) => {
+      const { userId } = req.body;
 
-    try {
-      const sql = `SELECT * FROM channels WHERE user_id = ?`;
-      const [results] = await conn.query(sql, userId);
-      if (results.length) res.status(200).json(results);
-      else {
-        notFoundChannel(res);
+      try {
+        const sql = `SELECT * FROM channels WHERE user_id = ?`;
+        const [results] = await conn.query(sql, userId);
+
+        if (results.length > 0) {
+          return res.status(200).json(results);
+        } else {
+          return notFoundChannel(res);
+        }
+      } catch (err) {
+        return res.status(500).json({ message: '서버 에러' });
       }
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: '서버 에러' });
-    }
-    res.status(400).end();
-  })
-  .post(async (req, res) => {
-    // 채널 개별 생성 = db 에 저장
-    const { name, userId } = req.body;
-    try {
-      if (name && userId) {
+    },
+  )
+  .post(
+    [
+      body('userId').notEmpty().isInt().withMessage('숫자 입력 필요'),
+      body('name').notEmpty().isString().withMessage('문자 입력 필요'),
+      validate,
+    ],
+    async (req, res) => {
+      // 채널 개별 생성 = db 에 저장
+      const { name, userId } = req.body;
+
+      try {
+        // 1. 유저 존재 여부 확인
+        const [user] = await conn.query('SELECT id FROM users WHERE id = ?', [
+          userId,
+        ]);
+        if (user.length === 0) {
+          return res
+            .status(404)
+            .json({ message: '존재하지 않는 userId 입니다.' });
+        }
+
+        // 2. 채널 생성
         const sql = 'INSERT INTO channels (name, user_id) VALUES (?, ?)';
-        const values = [name, userId];
-        const [results] = await conn.query(sql, values);
-        if (results.length) res.status(201).json(results);
-      } else {
-        res.status(400).json({ message: '요청 값이 잘못되었습니다.' });
+
+        const [results] = await conn.query(sql, [name, userId]);
+        if (results.affectedRows > 0) {
+          return res.status(201).json({ id: results.insertId, name, userId });
+        }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).end();
       }
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: '서버 에러' });
-    }
-  });
+    },
+  );
 
 router
   .route('/:id')
-  .get(async (req, res) => {
-    // 채널 개별 조회
-    let { id } = req.params;
-    id = parseInt(id);
+  .get(
+    [param('id').notEmpty().withMessage('채널 id 필요'), validate],
+    async (req, res) => {
+      // 채널 개별 조회
 
-    try {
-      const sql = `SELECT * FROM channels WHERE id = ?`;
-      const [results] = await conn.query(sql, id);
-      if (results.length) res.status(200).json(results);
-      else {
-        notFoundChannel(res);
+      const err = validationResult(req);
+      if (!err.isEmpty()) {
+        return res.status(400).json({ errors: err.array() });
       }
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: '서버 에러' });
-    }
-  })
-  .put((req, res) => {
-    let { id } = req.params;
-    id = parseInt(id);
 
-    var chanel = db.get(id);
-    if (chanel) {
-      const newTitle = req.body.channelTitle;
-      const oldTitle = chanel.channelTitle;
+      let { id } = req.params;
+      id = parseInt(id);
 
-      chanel.channelTitle = newTitle;
+      try {
+        const sql = `SELECT * FROM channels WHERE id = ?`;
+        const [results] = await conn.query(sql, id);
+        if (results.length) res.status(200).json(results);
+        else {
+          notFoundChannel(res);
+        }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: '서버 에러' });
+      }
+    },
+  )
+  .put(
+    [
+      param('id').notEmpty().withMessage('채널 id 필요'),
+      body('name').notEmpty().isString().withMessage('채널명 오류'),
+      validate,
+    ],
+    async (req, res) => {
+      // 채널 개별 수정
+      let { id } = req.params;
+      id = parseInt(id);
 
-      db.set(id, chanel);
-      res.status(200).json({
-        message: `채널명이 정상적으로 수정되었습니다. 기존 ${oldTitle} -> 수정 ${newTitle}`,
-      });
-    } else {
-      notFoundChannel();
-    }
-  }) // 채널 개별 수정
-  .delete((req, res) => {
-    let { id } = req.params;
-    id = parseInt(id);
+      let { name } = req.body;
 
-    var chanel = db.get(id);
+      try {
+        const sql = `UPDATE channels SET name = ? WHERE id = ?`;
+        const values = [name, id];
+        const [results] = await conn.query(sql, values);
+        if (results.affectedRows === 0) {
+          return res.status(400).end();
+        } else {
+          res.status(200).json(results);
+        }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: '서버 에러' });
+      }
+    },
+  )
+  .delete(
+    [param('id').notEmpty().withMessage('채널 id 필요'), validate],
+    async (req, res) => {
+      // 채널 개별 삭제
+      let { id } = req.params;
+      id = parseInt(id);
 
-    if (chanel) {
-      db.delete(id);
-      res.status(200).json({
-        message: `${chanel.channelTitle}이 정상적으로 삭제 되었습니다.`,
-      });
-    } else {
-      notFoundChannel();
-    }
-  }); // 채널 개별 삭제
+      const err = validationResult(req);
+      if (!err.isEmpty()) {
+        return res.status(400).json({ errors: err.array() });
+      }
+
+      try {
+        const sql = `DELETE FROM channels WHERE id = ?`;
+        const [results] = await conn.query(sql, id);
+
+        if (results.affectedRows === 0) {
+          return res.status(400).json(err.array());
+        } else {
+          res.status(200).json(results);
+        }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: '서버 에러' });
+      }
+    },
+  );
 
 function notFoundChannel(res) {
   res.status(404).json({
--- routes/users.js
+++ routes/users.js
@@ -1,109 +1,156 @@
 import express from 'express';
 const router = express.Router();
-
+import { body, param, validationResult } from 'express-validator';
 import conn from '../mariadb.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+// const dotenv =  require('dotenv');
+
+dotenv.config();
 
 router.use(express.json()); // http 외 모듈 'json' (json 을 body 에 꺼내서 쓰기위한 모듈)
 
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+
+  if (err.isEmpty()) {
+    return next();
+  } else {
+    return res.status(400).json({ errors: err.array() });
+  }
+};
+
 // 로그인
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/login',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+    body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
+    validate,
+  ],
+  async (req, res) => {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: '이메일 또는 비밀번호를 입력해주세요.' });
-  }
+    try {
+      const sql = 'SELECT * FROM users WHERE email = ?';
+      const values = [email];
+      const [results] = await conn.query(sql, values);
+      const loginUser = results[0];
 
-  try {
-    const sql = 'SELECT * FROM users WHERE email = ?';
-    const values = [email];
-    const [results] = await conn.query(sql, values);
-    const loginUser = results[0];
+      if (!loginUser) {
+        return res.status(403).json({ message: '회원 정보가 없습니다.' });
+      }
+      if (loginUser.password !== password) {
+        return res.status(400).json({ message: '비밀번호가 틀렸습니다.' });
+      }
+      const token = jwt.sign(
+        // 토큰 발급
+        { email: loginUser.email, name: loginUser.name },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: '30m', //유효기한 설정
+          issuer: 'jiwon',
+        },
+      );
+      res.cookie('token', token, { httpOnly: true });
+      console.log(token);
 
-    if (!loginUser) {
-      return res.status(404).json({ message: '회원 정보가 없습니다.' });
+      return res.status(200).json({
+        message: `${loginUser.name}님 로그인 되었습니다.`,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: '서버 에러 발생' });
     }
-    if (loginUser.password !== password) {
-      return res.status(400).json({ message: '비밀번호가 틀렸습니다.' });
-    }
-    return res.status(200).json({
-      message: `${loginUser.name}님 로그인 되었습니다.`,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: '서버 에러 발생' });
-  }
-});
+  },
+);
 
 // 회원 가입
-router.post('/join', async (req, res) => {
-  const { email, name, password, contact } = req.body;
+router.post(
+  '/join',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+    body('name').notEmpty().isString().withMessage('이름 확인 필요'),
+    body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
+    body('contact').notEmpty().isString().withMessage('번호 확인 필요'),
+    validate,
+  ],
+  async (req, res) => {
+    const { email, name, password, contact } = req.body;
 
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).json({ message: '입력 값을 다시 확인해주세요.' });
-  }
-
-  try {
-    const sql =
-      'INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)';
-    const values = [email, name, password, contact];
-
-    const [results] = await conn.query(sql, values);
-    res.status(201).json(results);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: '서버 에러' });
-  }
-});
+    try {
+      const sql =
+        'INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)';
+      const values = [email, name, password, contact];
+      const [results] = await conn.query(sql, values);
+      if (results.affectedRows === 0) {
+        return res.status(400).json(err.array());
+      } else {
+        res.status(200).json(results);
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: '서버 에러' });
+    }
+  },
+);
 
 // 회원 개별 조회
-router.get('/users', async (req, res) => {
-  const { email } = req.body;
+router.get(
+  '/users',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+    validate,
+  ],
+  async (req, res) => {
+    const { email } = req.body;
 
-  if (!email) {
-    return res.status(400).json({ message: 'EMAIL 정보 필요' });
-  }
-  try {
-    const sql = 'SELECT * FROM users WHERE email = ?';
-    const values = [email];
+    try {
+      const sql = 'SELECT * FROM users WHERE email = ?';
+      const values = [email];
 
-    const [results] = await conn.query(sql, values);
-    if (results.affectedRows === 0) {
-      res.status(404).json({
-        message: `회원 정보가 없습니다.`,
-      });
+      const [results] = await conn.query(sql, values);
+
+      if (results.length > 0) {
+        return res.status(200).json(results);
+      } else {
+        return res.status(400).json(err.array());
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: '서버 에러' });
     }
-    res.status(200).json(results);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: '서버 에러' });
-  }
-});
+  },
+);
 
 // 회원 개별 탈퇴
-router.delete('/users', async (req, res) => {
-  const { email } = req.body;
+router.delete(
+  '/users',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+    validate,
+  ],
+  async (req, res) => {
+    const { email } = req.body;
 
-  if (!email) {
-    return res.status(400).json({ message: 'EMAIL 정보 필요' });
-  }
+    try {
+      const sql = 'DELETE FROM users WHERE email = ?';
+      const values = [email];
 
-  try {
-    const sql = 'DELETE FROM users WHERE email = ?';
-    const values = [email];
+      const [results] = await conn.query(sql, values);
 
-    const [result] = await conn.query(sql, values);
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: '삭제할 유저가 없습니다.' });
+      if (results.affectedRows > 0) {
+        return res.status(200).json(results);
+      } else {
+        return res
+          .status(400)
+          .json({ message: '삭제 실패 (존재하지 않는 회원)' });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: '서버 에러' });
     }
-
-    return res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: '서버 에러' });
-  }
-});
+  },
+);
 
 export default router;
